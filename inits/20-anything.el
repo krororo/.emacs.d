66511741aa29a0d560e8aba137eff7cb0b1167ec
krororo <heavens.feel.7@gmail.com>
2014-02-25 13:02:03 +0900

    add custom anything for file

--- inits/20-anything.el
+++ inits/20-anything.el
@@ -4,3 +4,16 @@
 (require 'anything-myrurema)
 (global-set-key (kbd "C-c C-v") 'anything-git-files)
 (global-set-key (kbd "M-y") 'anything-show-kill-ring)
+
+(defun my-custom-anything-for-files ()
+  (interactive)
+  (let* ((git-source (and (anything-git-files:git-p)
+                          `(anything-git-files:modified-source
+                            anything-git-files:untracked-source
+                            anything-git-files:all-source)))
+         (sources `(anything-c-source-buffers+
+                    anything-c-source-recentf
+                    anything-c-source-files-in-current-dir+
+                    ,@git-source)))
+    (anything-other-buffer sources "*anything for files*")))
+(global-set-key (kbd "C-x C-g") 'my-custom-anything-for-files)
