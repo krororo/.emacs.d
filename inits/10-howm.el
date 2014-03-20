f9cf4aae19f989f4a9fac21e2e1705faed9567c1
krororo <heavens.feel.7@gmail.com>
2014-03-20 09:10:33 +0900

    howm の設定を変更

--- inits/10-howm.el
+++ inits/10-howm.el
@@ -1,4 +1,4 @@
 ;; howm
 (setq howm-menu-lang 'ja)
-(setq howm-directory "/mnt/hgfs/vm_share/howm")
-(autoload 'howm-menu "howm-mode" "Hitori Otegaru Wiki Modoki" t)
+(setq howm-directory (expand-file-name "~/howm"))
+(autoload 'howm-menu "howm" "Hitori Otegaru Wiki Modoki" t)
