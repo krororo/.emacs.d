524010cfe93ce85ff0e4810fa95d94519fd124f8
krororo <heavens.feel.7@gmail.com>
2014-04-03 17:38:23 +0900

    yasnippet のキーバインド変更

--- inits/10-yasnippet.el
+++ inits/10-yasnippet.el
@@ -4,6 +4,9 @@
       '("~/.emacs.d/snippets"
         "~/.emacs.d/elisp/yasnippet/snippets"))
 (yas-global-mode 1)
+(define-key yas-minor-mode-map (kbd "SPC") 'yas-expand)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map [(tab)] nil)
 (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
 (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
 (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
