d6848eaf20bb818f0439b58168d00ea88b1ab008
krororo <heavens.feel.7@gmail.com>
2014-04-21 16:20:43 +0900

    ruby-mode: 閉じカッコの位置調整

--- inits/20-ruby.el
+++ inits/20-ruby.el
@@ -10,5 +10,20 @@
              (define-key ruby-mode-map (kbd "C-c c r") 'anything-myrurema)
              (define-key ruby-mode-map (kbd "C-c d") 'flymake-display-err-minibuf)))
 
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
 ;; flymake setting
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
