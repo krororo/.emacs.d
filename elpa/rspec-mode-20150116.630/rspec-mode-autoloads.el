52ef07cdacd7a163658c8c1323de6dec7cae11bc
krororo <heavens.feel.7@gmail.com>
2015-01-19 10:30:57 +0900

    update rspec-mode

--- elpa/rspec-mode-20150116.630/rspec-mode-autoloads.el
+++ elpa/rspec-mode-20150116.630/rspec-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "rspec-mode" "rspec-mode.el" (21620 26281 957706
-;;;;;;  986000))
+;;;### (autoloads nil "rspec-mode" "rspec-mode.el" (21692 23795 416433
+;;;;;;  928000))
 ;;; Generated autoloads from rspec-mode.el
 
 (autoload 'rspec-mode "rspec-mode" "\
@@ -44,8 +44,8 @@ Returns true if the current buffer is a spec
 
 ;;;***
 
-;;;### (autoloads nil nil ("rspec-mode-pkg.el") (21620 26282 4178
-;;;;;;  856000))
+;;;### (autoloads nil nil ("rspec-mode-pkg.el") (21692 23795 491553
+;;;;;;  966000))
 
 ;;;***
 
