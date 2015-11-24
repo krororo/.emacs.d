89fa27730f62a7fc7e55d5b6b3c717fb3a7e9eb9
krororo <heavens.feel.7@gmail.com>
2015-11-24 12:44:03 +0900

    update dash

--- elpa/dash-20151021.113/dash-autoloads.el
+++ elpa/dash-20151021.113/dash-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("dash.el") (21913 55383 153537 282000))
+;;;### (autoloads nil nil ("dash.el") (22099 50878 641508 94000))
 
 ;;;***
 
